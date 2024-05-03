f76a17f61c635e81ccf7a47f9481f97db227cc85
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com.>
2024-05-03 18:47:32 +0200

    feat: add simple Text widget with app name

---
+++ lib/widgets/app_name_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppNameText extends StatelessWidget {
+  const AppNameText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('MangoVault');
+  }
+}
