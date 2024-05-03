c57f588383d315d60b761dd41d10b17872ecdb6c
Joaco-1321 <126332412+Joaco-1321@users.noreply.github.com.>
2024-05-03 18:49:43 +0200

    feat: add login screen
    basic login screen with username and password text fields
    
    the login method calls the authenticate method of the
    WebSocketManager so it sends the credentials to the server
    
    the callback receives the result of the authenticaion, either
    a success or a failure, if successful, it replaces the current
    screen with the HomeScreen, else, it shows a dialog informing
    the user

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:mangovault/model/user.dart';
+import 'package:mangovault/screens/home_screen.dart';
+import 'package:mangovault/web_socket_manager.dart';
+import 'package:mangovault/widgets/app_name_text.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _socketManager = WebSocketManager();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _width = 200.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const AppNameText(),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: _width,
+              child: TextField(
+                controller: _usernameController,
+                obscureText: false,
+                decoration: const InputDecoration(labelText: 'username'),
+              ),
+            ),
+            const SizedBox(height: 30.0),
+            SizedBox(
+              width: _width,
+              child: TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(labelText: 'password'),
+              ),
+            ),
+            const SizedBox(height: 30.0),
+            SizedBox(
+              width: _width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    onPressed: login,
+                    child: const Text('login'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void login() {
+    final username = _usernameController.text;
+    final password = _passwordController.text;
+
+    _socketManager.authenticate(
+      username,
+      password,
+      (success) {
+        if (success) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomeScreen(User(username), _socketManager),
+            ),
+          );
+        } else {
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: const Text('login failed'),
+              content: const Text('invalid username or password'),
+              actions: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('ok'),
+                ),
+              ],
+            ),
+          );
+        }
+      },
+    );
+  }
+}
